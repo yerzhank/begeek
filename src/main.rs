083073b79cd98ccd92eedb2faa4d92d65f43582d
Yerzhan Kudaibergenov <yerzhank@gmail.com>
2021-11-08 23:32:03 +0600

    change 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-   let x = 6;
-   println!("{}", x);
+//    let x = 6;
+//    println!("{}", x);
 
-   if x == 6 {
-       println!("ok");
-   }
+//    if x == 6 {
+//        println!("ok");
+//    }
 
-   let a = 10;
-   println!("{}", a);
+//    let a = 10;
+//    println!("{}", a);
 
+    println!("Угадай число!");
+
+
+    let secret_number = rand::thread_rng().gen_range(1..1001);
+    println!("You secret number {}", secret_number);
+
+    println!("Please input your guess:"); 
+
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed");
+    println!("You guessed {}", guess);
 }
